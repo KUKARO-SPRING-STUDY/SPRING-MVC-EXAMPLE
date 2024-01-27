97872ba3a9dcdbee2e5e25894b66c1923af446ff
coeat.k <coeat.k@kakaocorp.com>
2024-01-27 20:26:58 +0900

    feat:스프링 시큐리티 로그인로직 추가

--- src/main/java/org/example/springmvcexample/config/SecurityConfig.java
+++ src/main/java/org/example/springmvcexample/config/SecurityConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @Configuration
@@ -22,11 +22,11 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUserDetails(
-                User.builder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build())
+                        User.builder()
+                                .username("user")
+                                .password("password")
+                                .roles("USER")
+                                .build())
                 .build();
 
         return new InMemoryUserDetailsManager(user);
@@ -38,38 +38,29 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .cors() //cors 허용
-                .and()
-                .csrf() // csrf 끄기
-                .disable()
-                .exceptionHandling();
-//                .authenticationEntryPoint(unauthorizedHandler)
-//                .and()
-//                .sessionManagement() //(4)
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/auth/login/**").permitAll() // auth login 이하는 모두 허용
-//                .antMatchers("/**").authenticated() // 나머지는 auth를 탄다.
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .formLogin((login) -> login
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .permitAll())
+                .sessionManagement((session) -> session
+                        .sessionFixation().changeSessionId())
 //                .httpBasic(withDefaults())
-//                .formLogin(withDefaults());
-        return http.build();
+                .build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/", "/home", "/login");
     }
+
+
 }
--- src/main/java/org/example/springmvcexample/controller/HtmlController.java
+++ src/main/java/org/example/springmvcexample/controller/HtmlController.java
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HtmlController {
+
+    @GetMapping("/")
+    public String indexRoot() {
+        return "index";
+    }
     @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
     @GetMapping("/myself")
